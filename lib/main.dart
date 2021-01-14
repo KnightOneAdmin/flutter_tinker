e4dcda69b9aa3c70d4e06fea0aaa5e562df1c9ee
Knight_ <37442216knight@gmail.com>
2021-01-14 11:35:03 +0800

    *fix 1、bugfxi

--- lib/main.dart
+++ lib/main.dart
@@ -94,7 +94,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Flutter Tinker Demo',
+              'Flutter Tinker Demo-no_hotfix',
             ),
             Text(
               '$_counter',
